af7691a2ed77c7c8a7e9775b193751ff2df12bdb
Aasif Ali <aasifalisolangixwave@gmail.com>
2024-10-05 10:25:56 +0500

    I'm doing practice with git and github

---
+++ script.js
@@ -0,0 +1,6 @@
+let paragraph = document.getElementById('paragraph')
+paragraph.style.color = 'blue';
+paragraph.style.backgroundColor = 'red';
+paragraph.style.border = '2px solid black';
+paragraph.style.fontSize = '30px';
+// paragraph.innerHTML = 'Text change...';
